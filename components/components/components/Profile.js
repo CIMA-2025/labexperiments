ba4ce5d918a6f2d688794a75a9f6ffd199ce7ff4
igramirez <igramirez@tecmilenio.mx>
2022-04-27 12:04:42 -0500

    auth0 without token

--- components/components/components/Profile.js
+++ components/components/components/Profile.js
@@ -1,19 +1,27 @@
 import styles from './styles/Profile.module.css'
+import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Profile() {
-    return (
-        <>
-            <img
-                src="profile.svg"
-                alt="Imagen de perfil"
-                className={styles.image}
-            />
-            <div className={styles.text}>
-                <h1 className={styles.name}>Lorem impsum</h1>
-                <p className={styles.bio}>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta)
-                    Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500</p>
-            </div>
+    const { user, error, isLoading } = useUser();
 
-        </>
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>{error.message}</div>;
+
+    return (
+        user && (
+            <>
+                <img
+                    src="profile.svg"
+                    alt="Imagen de perfil"
+                    className={styles.image}
+                />
+                <div className={styles.text}>
+                    <h1 className={styles.name}>{user.name}</h1>
+                    <p className={styles.bio}>{user.email}</p>
+                </div>
+            </>
+        )
+
+
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 /*CSS for carousel*/
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { UserProvider } from '@auth0/nextjs-auth0';
+import React from 'react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
-
-export default MyApp
